394163f7888f1c1cee555d7301bde6dc93983ad0
Q520W <83735972+Q520W@users.noreply.github.com>
2022-05-18 14:06:29 -0500

    Add files via upload

---
+++ scripts/data.js
@@ -0,0 +1,63 @@
+let songs = [
+    {
+        name: 'Ghost',
+        path: 'assets/Ghost.mp3',
+        artist: 'Justin Bieber',
+        cover: 'assets/download-1.jpg'
+    },
+    {
+        name: 'Mood',
+        path: 'assets/Mood.mp3',
+        artist: '24k Goldn',
+        cover: 'assets/download-1.jpg'
+    },
+    {
+        name: 'Love Is Gone',
+        path: 'assets/Love Is Gone.mp3',
+        artist: 'SLANDER, Dylan Mathew',
+        cover: 'assets/download.jpg'
+    },
+    {
+        name: 'That Girl',
+        path: 'assets/That Girl.mp3',
+        artist: 'Olly Murs',
+        cover: 'assets/download.jpg'
+    },
+    {
+        name: 'Pubg',
+        path: 'assets/Pubg.mp3',
+        artist: 'Unknow',
+        cover: 'assets/download2.jpg'
+    },
+    {
+        name: 'Handclap',
+        path: 'assets/Hand Clap.mp3',
+        artist: 'Fitz and The Tantrums ',
+        cover: 'assets/download2.jpg'
+    },
+    {
+        name: 'Get Ready To Fight',
+        path: 'assets/Get Ready To Fight.mp3',
+        artist: 'Unknow',
+        cover: 'assets/download2.jpg'
+    },
+    {
+        name: '大鱼',
+        path: 'assets/大鱼.mp3',
+        artist: 'Shen Zhou 周深',
+        cover: 'assets/download3.jpg'
+    },
+    {
+        name: '孤城',
+        path: 'assets/孤城.mp3',
+        artist: 'Mr.Luo 洛先生',
+        cover: 'assets/download3.jpg'
+    },
+    {
+        name: 'Old Town Road',
+        path: 'assets/Old Town Road.mp3',
+        artist: 'Lil Nas X',
+        cover: 'assets/download4.jpg'
+    }
+]
+
